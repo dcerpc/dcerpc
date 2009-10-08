102f24f187a9dd2dba670daa315a286caa0a08fe
Brad Suinn <suinn@apple.com>
2009-10-08 11:13:59 -0600

    need this dummy file to get buildit to work

---
+++ dcerpc.xc.configurations/junk.c
@@ -0,0 +1,16 @@
+/*
+ *  junk.c
+ *  dcerpc
+ *
+ *  Created by Brad Suinn on 10/7/09.
+ *  Copyright 2009 Apple Inc. All rights reserved.
+ *
+ */
+
+
+int main( argc, argv )
+int argc;
+char *argv[];
+{
+	return 0;
+}
