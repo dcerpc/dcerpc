d7555647bbe7cbefdfb07896eb8c90ff498e1b46
Brian Koropoff <bkoropoff@likewise.com>
2009-11-21 00:13:13 +0000

    Merge lwmsg-task branch, plus a few fixes
    Double free in lwio client ConnectNamedPipe - Infinite loop in
    lwmsg_peer_task_rundown
    
    (dcerpc: r39297)
    (cherry picked from commit a00ba240d2d5d3fc065c78a398d7929dc08fc7b3)

--- dcerpc/ncklib/comsoc_smb.c
+++ dcerpc/ncklib/comsoc_smb.c
@@ -1256,7 +1256,9 @@ rpc__smb_socket_listen_thread(void* data)
         serr = NtStatusToErrno(
             LwIoCtxConnectNamedPipe(
                 smb->context,
-                smb->np));
+                smb->np,
+                NULL,
+                &status_block));
         if (serr)
         {
             SMB_SOCKET_LOCK(smb);
