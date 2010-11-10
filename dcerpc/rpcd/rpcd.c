vmware/likewise-open

commit 2c3d38337e93710cb9d49e9d1b5c04ac49177314

Fix for bug #9581 and the related issue with /var/rpc on FreeBSD amd64.
CR:  bkoropoff

(dcerpc: r40053)

dcerpc/configure.in
@@ -163,6 +163,11 @@ RPC_ARG_DEFINE(dcom, ENABLE_DCOM, no,[Enable experimental DCOM support])
 AM_CONDITIONAL(ENABLE_DCOM, test x$rpc_arg_dcom = yes)
 AM_CONDITIONAL(TARGET_OS_LINUX, test x$target_os = xlinux-gnu)
 
+
+AS_AC_EXPAND(rpc_dir, ["$localstatedir/rpc"])
+AC_DEFINE_UNQUOTED([RPC_C_NP_DIR], ["$rpc_dir"], [named pipes directory])
+AC_DEFINE_UNQUOTED([RPC_C_UXD_DIR], ["$rpc_dir"], [unix domain sockets directory])
+
 #####################################################################
 ## COMPONENT: threads
 

dcerpc/rpcd/rpcd.c
@@ -1172,10 +1172,15 @@ int main(int argc, char *argv[])
     /*
      * Ensure permissions on /var/rpc directory
      */
-    if (chmod("/var/rpc", S_IRUSR | S_IWUSR | S_IXUSR) != 0)
+    if (chmod(RPC_C_NP_DIR, S_IRUSR | S_IWUSR | S_IXUSR) != 0)
     {
-        printf("(rpcd) could not change permissions on /var/rpc directory...\n");
-        exit(1);
+        if (errno != ENOENT ||
+            mkdir(RPC_C_NP_DIR, S_IRUSR | S_IWUSR | S_IXUSR) != 0)
+        {
+
+            printf("(rpcd) could not change permissions on " RPC_C_NP_DIR " directory...\n");
+            exit(1);
+        }
     }
 
     /*

dcerpc/ncklib/ncklib.mk
@@ -1,5 +1,5 @@
 NCK_INCLUDES=-I$(top_srcdir)/ncklib -I$(top_srcdir)/ncklib/include/$(target_os)
-NCK_DEFINES=-DNCK -DRPC_C_NP_DIR=\"$(localstatedir)/rpc\" -DRPC_C_UXD_DIR=\"$(localstatedir)/rpc\" 
+NCK_DEFINES=-DNCK
 
 # It would seem that putting the defines in CFLAGS is a hack.
 AM_CPPFLAGS+=$(NCK_INCLUDES)

dcerpc/dcerpc

commit 24f837cea399d73c8d1d6cdd08b80e395ea99f08

Allow ncalrpc and ncacn_np directory paths to differ.
We already allowed this, but 2c3d38337e93710cb9d49e9d1b5c04ac49177314
broke us by forcing the definitions of RPC_C_*_DIR in configure.
This restores their configurability which is moved into configure.

Radar: <rdar://problem/7554050>

dcerpc.xc.configurations/base_config.xcconfig
@@ -40,12 +40,11 @@ RPC_OPTIMIZATION_Debug=0
 RPC_OPTIMIZATION_Release=s
 GCC_OPTIMIZATION_LEVEL=$(RPC_OPTIMIZATION_$(CONFIGURATION))
 
-RPC_SOCKET_PATHS=RPC_C_UXD_DIR=\"/var/rpc/ncalrpc\" RPC_C_NP_DIR=\"/var/rpc/ncacn_np\"
 RPC_CATALOG_PATH=CATALOG_DIR=\"/System/Library/PrivateFrameworks/DCERPC.framework/Resources/Catalogs\"
 RPC_IDIR_PATH=DEFAULT_IDIR=\"/usr/local/include\"
 RPC_MODULES_PATH=IMAGE_DIR=\"/System/Library/PrivateFrameworks/DCERPC.framework/Resources/Modules\"
 
-RPC_PATH_DEFINES=$(RPC_SOCKET_PATHS) $(RPC_CATALOG_PATH) $(RPC_IDIR_PATH) $(RPC_MODULES_PATH)
+RPC_PATH_DEFINES=$(RPC_CATALOG_PATH) $(RPC_IDIR_PATH) $(RPC_MODULES_PATH)
 
 RPC_DERIVED_HEADERS=$(SHARED_DERIVED_FILE_DIR) $(SHARED_DERIVED_FILE_DIR)/dce
 RPC_INTERNAL_HEADERS=$(SRCROOT)/dcerpc/include

dcerpc.xc.configurations/buildconf_configure
@@ -3,7 +3,10 @@ if test ! -e "${SRCROOT}/dcerpc/configure"; then
     cd "${SRCROOT}/dcerpc"
     echo "Running buildconf and configure"
     ./buildconf
-    ./configure --disable-dependency-tracking --enable-idldumpers
+    ./configure --disable-dependency-tracking --enable-idldumpers \
+	rpcd_cv_rpc_c_np_dir=/var/rpc/ncacn_np \
+	rpcd_cv_rpc_c_uxd_dir=/var/rpc/ncalrpc
+
 else
     echo "buildconf and configure have already been executed"
 fi

dcerpc/configure.in
@@ -178,8 +178,17 @@ AC_CHECK_HEADERS(sys/kauth.h sys/syscall.h)
 
 
 AS_AC_EXPAND(rpc_dir, ["$localstatedir/rpc"])
-AC_DEFINE_UNQUOTED([RPC_C_NP_DIR], ["$rpc_dir"], [named pipes directory])
-AC_DEFINE_UNQUOTED([RPC_C_UXD_DIR], ["$rpc_dir"], [unix domain sockets directory])
+
+AC_CACHE_CHECK([for named pipes directory],
+	[rpcd_cv_rpc_c_np_dir],
+	[rpcd_cv_rpc_c_np_dir="$rpc_dir"])
+
+AC_CACHE_CHECK([for unix domain sockets directory],
+	[rpcd_cv_rpc_c_uxd_dir],
+	[rpcd_cv_rpc_c_uxd_dir="$rpc_dir"])
+
+AC_DEFINE_UNQUOTED([RPC_C_NP_DIR], ["$rpcd_cv_rpc_c_np_dir"], [named pipes directory])
+AC_DEFINE_UNQUOTED([RPC_C_UXD_DIR], ["$rpcd_cv_rpc_c_uxd_dir"], [unix domain sockets directory])
 
 #####################################################################
 ## COMPONENT: threads

dcerpc/rpcd/rpcd.c
@@ -3,6 +3,7 @@
  * (c) Copyright 1989 OPEN SOFTWARE FOUNDATION, INC.
  * (c) Copyright 1989 HEWLETT-PACKARD COMPANY
  * (c) Copyright 1989 DIGITAL EQUIPMENT CORPORATION
+ * Portions Copyright (c) 2010 Apple Inc. All rights reserved
  * To anyone who acknowledges that this file is provided "AS IS"
  * without any express or implied warranty:
  *                 permission to use, copy, modify, and distribute this
@@ -1091,6 +1092,24 @@ error:
     goto cleanup;
 }
 
+static void
+rpcd_verify_sockets_directory(
+	const char * path)
+{
+
+    if (chmod(path, S_IRUSR | S_IWUSR | S_IXUSR) != 0)
+    {
+        if (errno != ENOENT ||
+            mkdir(path, S_IRUSR | S_IWUSR | S_IXUSR) != 0)
+        {
+
+            printf("(rpcd) could not change permissions on %s directory...\n",
+		    path);
+            exit(1);
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     error_status_t  status, listen_status, network_status;
@@ -1145,17 +1164,14 @@ int main(int argc, char *argv[])
     if (! STATUS_OK(&status)) exit(1);
 
     /*
-     * Ensure permissions on /var/rpc directory
+     * Ensure permissions on pipes directory and on unix sockets
+     * directory if it's different.
      */
-    if (chmod(RPC_C_NP_DIR, S_IRUSR | S_IWUSR | S_IXUSR) != 0)
-    {
-        if (errno != ENOENT ||
-            mkdir(RPC_C_NP_DIR, S_IRUSR | S_IWUSR | S_IXUSR) != 0)
-        {
 
-            printf("(rpcd) could not change permissions on " RPC_C_NP_DIR " directory...\n");
-            exit(1);
-        }
+    rpcd_verify_sockets_directory(RPC_C_NP_DIR);
+    if (strcmp(RPC_C_NP_DIR, RPC_C_UXD_DIR) != 0)
+    {
+	rpcd_verify_sockets_directory(RPC_C_UXD_DIR);
     }
 
     /*